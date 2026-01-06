1ae5617664e3d9ca01d913d641fcacbe2acfcfc3
luiz felipe <lf92416@gmail.com>
2026-01-05 22:49:06 -0300

    Configuração do DbContext via appsettings e ref. Infra
    Removido connection string do código-fonte e configurado via appsettings.Development.json. Ajustado Program.cs para usar o connection string nomeado. Adicionada referência ao projeto PadocaGestor.Infrastructure no csproj. Melhorias de segurança e organização.

--- PadocaGestor/Program.cs
+++ PadocaGestor/Program.cs
@@ -1,15 +1,21 @@
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Options;
+using PadocaGestor.Infrastructure.Database;
 using Scalar.AspNetCore;
 
 var builder = WebApplication.CreateBuilder(args);
+
 
 // Add services to the container.
 
 builder.Services.AddControllers();
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
+builder.Services.AddDbContext<PadocaContext>(opt=> opt.UseNpgsql(builder.Configuration.GetConnectionString("Padoca")));
 
 var app = builder.Build();
+
+
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
