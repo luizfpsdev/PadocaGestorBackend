9a0e6be6b9233ae86d000ad6ce75739d7f4a0d14
luiz felipe <lf92416@gmail.com>
2026-01-07 21:50:09 -0300

    Implementa Repository e UnitOfWork genéricos
    Adiciona classes Repository<TEntity> e UnitOfWork para abstração
    de acesso a dados com Entity Framework Core. Inclui métodos
    básicos de CRUD e gerenciamento de contexto, seguindo os
    padrões Repository e Unit of Work.

---
+++ PadocaGestor.Infrastructure/Repository/UnitOfWork.cs
@@ -0,0 +1,27 @@
+﻿using PadocaGestor.Infrastructure.Database;
+using PadocaGestor.Infrastructure.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PadocaGestor.Infrastructure.Repository
+{
+    public class UnitOfWork : IDisposable
+    {
+
+        public UnitOfWork(PadocaContext context)
+        {
+            Context = context;
+        }
+
+        public PadocaContext Context { get; }
+
+        private Repository<Fornecedor> FornecedorRepository;
+        private Repository<Funcionario> FuncionarioRepository;
+
+        public void Dispose()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
