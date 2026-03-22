a404286d6a7133b983dccd0839d3799cae6cdbab
luiz felipe <lf92416@gmail.com>
2026-03-22 11:37:06 -0300

    feature: alteraçao nome coluna

---
+++ PadocaGestor.Infrastructure/Migrations/20260322143611_ajustePrecoVenda2.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace PadocaGestor.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class ajustePrecoVenda2 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "preco",
+                table: "produto",
+                newName: "preco_venda");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "preco_venda",
+                table: "produto",
+                newName: "preco");
+        }
+    }
+}
