93626a6992f58bd061adca585b5512055839bad1
luiz felipe <lf92416@gmail.com>
2026-04-11 18:36:16 -0300

    migrations novos campos

---
+++ PadocaGestor.Infrastructure/Migrations/20260329193352_adicaoCamposPrecificacao.cs
@@ -0,0 +1,62 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace PadocaGestor.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class adicaoCamposPrecificacao : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<decimal>(
+                name: "Markup",
+                table: "receitas",
+                type: "numeric",
+                nullable: false,
+                defaultValue: 0m);
+
+            migrationBuilder.AddColumn<decimal>(
+                name: "PrecoVenda",
+                table: "receitas",
+                type: "numeric",
+                nullable: false,
+                defaultValue: 0m);
+
+            migrationBuilder.AddColumn<int>(
+                name: "TipoPrecificacao",
+                table: "receitas",
+                type: "integer",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Email",
+                table: "fornecedores",
+                type: "text",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Markup",
+                table: "receitas");
+
+            migrationBuilder.DropColumn(
+                name: "PrecoVenda",
+                table: "receitas");
+
+            migrationBuilder.DropColumn(
+                name: "TipoPrecificacao",
+                table: "receitas");
+
+            migrationBuilder.DropColumn(
+                name: "Email",
+                table: "fornecedores");
+        }
+    }
+}
---
+++ PadocaGestor.Infrastructure/Migrations/20260329202021_descricao.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace PadocaGestor.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class descricao : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Descricao",
+                table: "receitas",
+                type: "text",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Descricao",
+                table: "receitas");
+        }
+    }
+}
